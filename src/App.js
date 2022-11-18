336f4fae422b23bd01d6796a2300292f768dfeb9
MatteoG <honrop.giochi@gmail.com>
2022-11-18 10:30:26 +0100

    inz 2nd lesson

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div>
+      I nostri viaggi
+    </div>
+    
+  );
+}
+//Tante componenti : titolo, immagine, descrizione, durata, prezzo, bottone interessa ..a.. (...poi con +1)
+export default App;
